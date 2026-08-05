e4422df33eea7bc00064e492847b6d8e6a53556d
kanakpurts1965 <kanakpurts1965@gmail.com>
2026-08-06 03:55:10 +0530

    Initialize Firebase app and set up authentication

--- assets/js/firebase.js
+++ assets/js/firebase.js
@@ -1,1 +1,29 @@
-// firebase.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
+
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
+
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
+
+const firebaseConfig={
+
+apiKey:"YOUR_API_KEY",
+
+authDomain:"YOUR_PROJECT.firebaseapp.com",
+
+projectId:"YOUR_PROJECT_ID",
+
+storageBucket:"YOUR_PROJECT.firebasestorage.app",
+
+messagingSenderId:"YOUR_SENDER_ID",
+
+appId:"YOUR_APP_ID"
+
+};
+
+const app=initializeApp(firebaseConfig);
+
+export const auth=getAuth(app);
+
+export const db=getFirestore(app);
+
+export default app;
